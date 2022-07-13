beaf5332aa770b2e6723c34962607719813caf29
Joao <bcastillo-2022474@kinal.edu.gt>
2022-07-13 09:39:07 -0600

    Playing with GridLayout

---
+++ src/main/gridLayout/Main.java
@@ -0,0 +1,21 @@
+package main.gridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(3, 3, 10, 20));
+        frame.setSize(500, 500);
+
+        for (int i = 1; i <= 10; i++) {
+            frame.add(new JButton("Im a Button Number " + i));
+        }
+
+        frame.setVisible(true);
+    }
+}
