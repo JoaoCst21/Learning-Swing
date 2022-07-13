e36e1f34b7df9c899c74d85cc1898b52e4e3b968
Joao <bcastillo-2022474@kinal.edu.gt>
2022-07-13 09:27:41 -0600

    Playing with flowLayout

---
+++ src/main/flowLayout/Main.java
@@ -0,0 +1,33 @@
+package main.flowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLayout(new FlowLayout(FlowLayout.LEADING, 40, 30));
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(300, 300));
+        panel.setBackground(Color.yellow);
+//        panel.setLayout(new FlowLayout()); // Default
+//        panel.setVisible(true);
+
+        for (int i = 0; i < 10; i++) {
+            panel.add(new JButton("Hello im " + i));
+        }
+
+        frame.add(panel);
+
+    }
+}
